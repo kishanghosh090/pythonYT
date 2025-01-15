f5144ccd3b84229f76a71537e5acf1f502801e71
kishanghosh090 <kishanghosh090@gmail.com>
2025-01-15 11:48:22 +0530

    basic of python

---
+++ chapter1-PS/problem1.py
@@ -0,0 +1,22 @@
+# problem1----------------------
+
+# print('''
+# this is akash ghosh
+# this is akash ghosh
+# this is akash ghosh
+# ''')
+
+# pyttsx3 modules used for text to speech--------------
+# import pyttsx3
+
+# engine = pyttsx3.init()
+# engine.say("I will speak this text")
+# engine.runAndWait()
+
+# os modules used for operating system----------------
+import os
+
+directory_path = "/home/krg/Desktop/PYTHONYT/pythonYT/"
+
+contents = os.listdir(directory_path)
+print(contents)
---
+++ first.py
@@ -0,0 +1,12 @@
+import pyjokes 
+
+# printing jokes [single line comments]
+
+"""
+this is 
+multi line
+comment
+"""
+
+joke = pyjokes.get_joke()
+print(joke)
