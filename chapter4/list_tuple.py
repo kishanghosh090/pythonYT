f541f907c12ba9b9d2e05178f0a6d2eab229d129
kishanghosh090 <kishanghosh090@gmail.com>
2025-01-16 15:20:52 +0530

    list and tuples are completed

--- chapter4/list_tuple.py
+++ chapter4/list_tuple.py
@@ -1,7 +1,7 @@
 # lists are mutable and it can be changed
 
 chai = ["lemon tea", "alychi tea","zinzer tea", "black tea"]
-print(chai)
+# print(chai)
 # print(type(chai))
 # print(chai.copy())
 
@@ -17,4 +17,18 @@ L1 = [1,2,3,45,54,1,34,545]
 
 L1.insert(2, "kishan")
 L1.pop(2)# removes the element at index 2 and returns it
-print(L1)
+L1.remove(45) # removes a perticuler value
+
+# print(L1)
+
+
+# ***  tuples are immutable ******
+
+t1 = (1,2,1,3,46,76,"kishan","coffee")
+# t1[2] = 4 # throws error as touples are immutable in python
+
+# print(t1.count(1))
+
+ # tuple methods------
+print(t1.index("kishan"))
+print(t1.__add__((1,2,3)),t1)
