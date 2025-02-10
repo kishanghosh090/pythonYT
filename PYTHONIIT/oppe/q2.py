8f0aa12fe155dcc91f623287eb78eac6339ef9e3
kishanghosh090 <kishanghosh090@gmail.com>
2025-02-10 19:27:53 +0530

    added

---
+++ PYTHONIIT/oppe/q2.py
@@ -0,0 +1,43 @@
+# str = "ksihan"
+# # str.reverse()
+
+# str2 = str[::-1]
+# print(str2,len(str) %2 == 0)
+
+# def is_odd_length_palindrome(s: str) -> bool:
+#     '''Check if a string is a palindrome with odd length.
+
+#     Args:
+#         s : str - input string
+
+#     Returns:
+#         bool - True if s is a palindrome with odd length, False otherwise.
+#     '''
+#     strRev = s[::-1]
+#     print(strRev == s)
+    
+#     if((s == strRev) and (len(s)%2 == 0)):
+#         return True
+        
+#     else:
+#         return False
+    
+    
+# print(is_odd_length_palindrome("racecar"))
+
+
+
+# n = int(input())
+
+# for i in range(n):
+#     vowel = "aeiouAEIOU"
+#     newStr = ""
+#     str1 = input()
+#     for char in str1:
+#         if char in vowel:
+#             newStr = newStr + char.upper()
+#         else:
+#             newStr = newStr + char.lower()
+
+#     print(newStr)
+
