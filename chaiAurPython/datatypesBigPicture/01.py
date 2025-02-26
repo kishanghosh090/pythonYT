770e4786afaa2244d4e8c56c76342a047584a4bf
kishanghosh090 <kishanghosh090@gmail.com>
2025-02-26 23:33:51 +0530

    added

---
+++ chaiAurPython/datatypesBigPicture/01.py
@@ -0,0 +1,18 @@
+# data types(object types in python) in python big picture overview
+
+# 1. Numbers : int, float, complex numbers(1+2j)
+# 2. Strings : 'hello', "hello", '''hello''', """hello"""
+# 3. Lists: [1, 2, 3, 4, 5]
+# 4. Tuples: (1, 2, 3, 4, 5)
+# 5. Dictionaries: {'name': 'kishan', 'age': 25}
+# 6. Sets: {1, 2, 3, 4, 5}
+# 7. Booleans: True, False
+# 8. None
+# 9: Functions, Classes, Modules
+
+
+
+x = 10
+y = x
+
+myList = [1, 2, 3, 4, 5,"kishan","rana","ghosh",3.14,1.0,4.0]
