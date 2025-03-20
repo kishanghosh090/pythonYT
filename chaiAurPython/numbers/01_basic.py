09316b35cff83f28502e9c50c602737fa412875e
kishanghosh090 <kishanghosh090@gmail.com>
2025-03-20 21:23:14 +0530

    added

---
+++ chaiAurPython/numbers/01_basic.py
@@ -0,0 +1,20 @@
+# numbers in python
+
+x = 234
+y = 434
+z = 234
+
+
+name = "chai" + "aur" + "code"
+
+print(x,y,z)
+
+rem = x % y
+print(rem)
+
+power = x ** 200
+print(power)
+repr(power)
+str(power)
+print(5 == 5.0)
+print(x<y<z) # x<y and y<z
---
+++ chaiAurPython/string/string.py
@@ -0,0 +1,23 @@
+chai = "alaychi tea chai chai "
+
+first_char = chai[0]
+
+print(first_char)
+slice_chai = chai[1:5]
+print(slice_chai)
+# print(chai.upper())
+# print(chai.strip())
+
+chia = "Lemon, Ginger, Masala, Mint, Cardamom"
+print(chia.split(",")) # ['Lemon', ' Ginger', ' Masala', ' Mint', ' Cardamom']
+
+print(chai.find("a")) # 1
+
+lists = ["Lemon", "Ginger", "Masala", "Mint", "Cardamom"]
+
+print(" ".join(lists)) # Lemon Ginger Masala Mint Cardamom
+
+chai = "he said , maasala chia is \"awsome\""
+print(chai) # he said , maasala chia is "awsome"
+
+print(r"he said , maasala chia is \"awsome\"") # he said , maasala chia is \"awsome\"
